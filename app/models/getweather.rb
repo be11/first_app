e815cb4fcd10b49b77da80bb310ceb769d0ca946
Yuuna Kurita <yuuna.m@gmail.com>
2013-09-28 12:19:20 +0900

    コメント追加 コメントをみて新しく作り直してみてください

--- app/models/getweather.rb
+++ app/models/getweather.rb
@@ -1,3 +1,9 @@
+# -*- coding: undecided -*-
 class Getweather < ActiveRecord::Base
+  
+  #クラス名がよくない
+  #モデルはDBそのものを表すためにmodel名はWeatherとすべき
+  #http://d.hatena.ne.jp/jyouji1979/20110521/1305998761
+
   attr_accessible :clouds, :description, :get_time, :humidity, :lat, :lon, :pref_id, :pressure, :sunrize, :sunset, :temp, :temp_max, :temp_min, :windspeed
 end
